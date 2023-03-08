8eac19cfc643bc14242c01321cc46e493ade966c
arafakst <arafakst@gmail.com>
2023-03-08 17:36:57 +0600

    get sum of odd numbers

---
+++ oddnum.js
@@ -0,0 +1,31 @@
+function getSumOfAnArray(numbers){
+    let sum  = 0;
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+    const element = numbers[index];
+    sum = sum + element;
+    // console.log(index, element, sum);
+    }
+    return sum;
+    
+}
+
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers = [];
+    for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            console.log(index,element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+
+const myNumbers = [12, 65, 34, 67, 87,91];
+const oddNumbers= getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumbersSum = getSumOfAnArray(oddNumbers);
+
+console.log('odd number sum',oddNumbersSum);
